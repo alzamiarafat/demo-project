6e1c0c2a4c23d70a812d7dedc85ccdff9f578b1b
AL Zami Arafat <alzamiarafat00@gmail.com>
2021-11-08 18:51:59 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';  
 class App extends Component{  
    render(){
-      var myStyle = {  
+     var i = 5;
+     var myStyle = {
         fontSize: 80,  
         fontFamily: 'Courier',  
         color: '#003300'  
       }  
       return(  
-         <div>  
-             <p className="text-danger" style={myStyle}>Add = {15+20}</p>
-             <p className="text-info">Sub = {15-20}</p>
-             <p className="text-success">Div = {15/20}</p>
-             <p className="text-danger">Mul = {1*20}</p>
-         </div>  
+        <div>  
+            <p className="text-danger" style={myStyle}>Add = {15+20}</p>
+            <p className="text-info">Sub = {15-20}</p>
+            <p className="text-success">Div = {15/20}</p>
+            <p className="text-danger">Mul = {1*20}</p>
+            <p className="text-danger">{ i == 2 ? 'false' : 'true'}</p>
+        </div>  
       );  
    }  
 }  
