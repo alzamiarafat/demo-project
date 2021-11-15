b6e87c2d75143c7561c202aaf4e3feeadb88299a
AL Zami Arafat <alzamiarafat00@gmail.com>
2021-11-15 15:45:07 +0600

    user create and user list show

--- src/components/User/UserCreate.js
+++ src/components/User/UserCreate.js
@@ -1,37 +1,55 @@
 import React, { Component } from 'react';
 import SideBar from "../Layout/SideBar/SideBar";
+import axios from "axios";
+import swal from 'sweetalert';
+import { useNavigate } from "react-router-dom";
 
+function UserCreate() {
+    const history = useNavigate();
 
-export class UserCreate extends Component {
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="row flex-nowrap">
-                    <SideBar />
-                    <div className="col py-3">
-                        <form className="card" onSubmit={this.formSubmit}>
-                            <div className="card-body">
-                                <legend>Create User</legend>
-                                <div className="mb-3">
-                                    <label htmlFor="exampleInputUsername" className="form-label">User Name</label>
-                                    <input type="text" className="form-control" name="username" />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                    <input type="email" className="form-control" name="email" />
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                    <input type="password" className="form-control" name="password" />
-                                </div>
-                                <button type="submit" className="btn btn-primary">Submit</button>
+    return (
+        <div className="container-fluid">
+            <div className="row flex-nowrap">
+                <SideBar />
+                <div className="col py-3">
+                    <form className="card" onSubmit={formSubmit}>
+                        <div className="card-body">
+                            <legend>Create User</legend>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputUsername" className="form-label">User Name</label>
+                                <input type="text" className="form-control" name="username" placeholder="Enter Username" />
                             </div>
-                        </form>
-                    </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                                <input type="email" className="form-control" name="email" placeholder="Enter Email Address" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                                <input type="password" className="form-control" name="password" placeholder="Enter Password" />
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
                 </div>
             </div>
-        )
+        </div>
+    )
+
+    function formSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        const email = data.get('email');
+        const username = data.get('username');
+        const password = data.get('password');
+    
+        axios.post(`https://6190abddf6bf450017484c7d.mockapi.io/fakeData`, { username, email, password }).then(() => {
+            swal("Good job!", "You clicked the button!", "success").then(() => {
+                event.target.reset();
+                history('/users')
+            });
+        })
     }
 }
+
 
 export default UserCreate
--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -1,22 +1,102 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import SideBar from '../Layout/SideBar/SideBar';
+import axios from "axios";
 
-export class UserList extends Component {
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="row flex-nowrap">
-                    <SideBar />
-                    <div className="col py-3">
-                        <h3>User List</h3>
-                        <Link to="create" className="nav-link align-middle px-0"><i className="fas fa-certificate"></i> <span className="ms-1 d-none d-sm-inline">Test Item</span></Link>
+function UserList() {
+    const [APIData, setAPIData] = useState([]);
+    useEffect(() => {
+        axios.get(`https://6190abddf6bf450017484c7d.mockapi.io/fakeData`)
+            .then((response) => {
+                setAPIData(response.data);
+            })
+    }, [])
+    return (
+        <div className="container-fluid">
+            <div className="row flex-nowrap">
+                <SideBar />
+                <div className="col py-3">
+                    <h3>User List</h3>
+                    <div className="card">
+                        <div className="card-body">
+                            <div className="row">
+                                <div className="col-12 text-end">
+                                    <Link to="create" className="btn btn-outline-primary"><i className="fas fa-plus"></i> <span>Add New</span></Link>
+                                </div>
+                            </div>
+                            <div className="row">
+
+                                <div className="col-12">
+                                    <table className="table table-bordered mt-4">
+                                        <thead>
+                                            <tr className="text-center">
+                                                <th>SL</th>
+                                                <th>Username</th>
+                                                <th>Email</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {APIData.map((user, index) => (
+                                                <tr className="text-center" key={user.id}>
+                                                    <td>{index+1}</td>
+                                                    <td>{user.username}</td>
+                                                    <td>{user.email}</td>
+                                                    <td>
+                                                        <Link to={user.id} className="btn btn-outline-warning"><i className="fas fa-eye"></i></Link> &nbsp;
+                                                        <Link to={user.id} className="btn btn-outline-success"><i className="fas fa-edit"></i></Link>  &nbsp;
+                                                        <Link to={user.id} className="btn btn-outline-danger"><i className="fas fa-trash"></i></Link>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
+
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
+        </div>
 
-        )
-    }
+    )
 }
 
 export default UserList
+
+
+// function Users() {
+//     const [APIData, setAPIData] = useState([]);
+//     useEffect(() => {
+//         axios.get(`https://6190abddf6bf450017484c7d.mockapi.io/fakeData`)
+//             .then((response) => {
+//                 console.log(response.data)
+//                 setAPIData(response.data);
+//             })
+//     }, [])
+// }
+
+
+// const [APIData, setAPIData] = useState([]);
+// useEffect(() => {
+//     axios.get(`https://6190abddf6bf450017484c7d.mockapi.io/fakeData`)
+//         .then((response) => {
+//             console.log(response.data)
+//             // setAPIData(response.data);
+//         })
+// }, [])
+
+// export class UserList extends Component {
+//     // constructor(props) {
+//     //     super(props)
+//     // };
+//     render() {
+
+//     }
+// }
+
+
+
+// export default UserList
