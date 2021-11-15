84532e32251d690d929a067c98a3ed642d29a470
AL Zami Arafat <alzamiarafat00@gmail.com>
2021-11-15 18:39:47 +0600

    register component convert class to function

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,52 +1,51 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { useAuth } from '../../contexts/Auth';
-require('./Register.css')
+import './Register.css';
+// require('./Register.css');
 
+function Register() {
+    // const { signUp } = useAuth();
 
-export class Register extends Component {
-    render() {
-        const { signUp } = useAuth();
-        return (
-            <div className="reg-bg">
-                <div className="registration-form">
-                    <form>
-                        <div className="form-icon">
-                            <span><i className="fas fa-user-plus"></i></span>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control item" id="username" placeholder="Username" />
-                        </div>
-                        <div className="form-group">
-                            <input type="password" className="form-control item" id="password" placeholder="Password" />
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control item" id="email" placeholder="Email" />
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control item" id="phone-number" placeholder="Phone Number" />
-                        </div>
-                        <div className="form-group">
-                            <input type="text" className="form-control item" id="birth-date" placeholder="Birth Date" />
-                        </div>
-                        <div className="form-group">
-                            <button type="button" className="btn btn-block create-account">Create Account</button>
-                        </div>
-                        <p href="#" className="mb-0 pull-right">Already have an account? <Link to="/login">Login</Link></p>
+    return (
+        <div className="reg-bg">
+            <div className="registration-form">
+                <form>
+                    <div className="form-icon">
+                        <span><i className="fas fa-user-plus"></i></span>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control item" id="username" placeholder="Username" />
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control item" id="password" placeholder="Password" />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control item" id="email" placeholder="Email" />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control item" id="phone-number" placeholder="Phone Number" />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control item" id="birth-date" placeholder="Birth Date" />
+                    </div>
+                    <div className="form-group">
+                        <button type="button" className="btn btn-block create-account">Create Account</button>
+                    </div>
+                    <p href="#" className="mb-0 pull-right">Already have an account? <Link to="/login">Login</Link></p>
+                </form>
 
-                    </form>
-                    <div className="social-media">
-                        <h5>Sign up with social media</h5>
-                        <div className="social-icons">
-                            <a href="#"><i className="fab fa-facebook-f" title="Facebook"></i></a>
-                            <a href="#"><i className="fab fa-google" title="Google"></i></a>
-                            <a href="#"><i className="fab fa-twitter" title="Twitter"></i></a>
-                        </div>
+                <div className="social-media">
+                    <h5>Sign up with social media</h5>
+                    <div className="social-icons">
+                        <a href="#"><i className="fab fa-facebook-f" title="Facebook"></i></a>
+                        <a href="#"><i className="fab fa-google" title="Google"></i></a>
+                        <a href="#"><i className="fab fa-twitter" title="Twitter"></i></a>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Register
