50d609026ac501954afcd1f9a3b81f88643e5002
Al Zami Arafat <alzamiarafat00@gmail.com>
2021-11-24 10:53:54 +0600

    add contant header menu

--- src/components/Dashboard/Layout/Header/index.js
+++ src/components/Dashboard/Layout/Header/index.js
@@ -1,39 +1,109 @@
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import { AppBar, Grid, IconButton, InputBase, InputLabel, TextField, Toolbar } from '@mui/material';
-import { Badge } from 'antd';
+import {
+    AccountCircle,
+    NotificationsNone,
+    MailOutline,
+    Settings,
+    Logout
+} from '@mui/icons-material';
+import {
+    AppBar,
+    Grid,
+    IconButton,
+    InputBase,
+    ListItemIcon,
+    Toolbar,
+    MenuItem,
+    Menu,
+    Badge
+} from '@mui/material';
 import React from 'react';
 import useStyles from './style';
+import { useAuth } from '../../../../contexts/Auth';
+import { useNavigate } from 'react-router';
 
 const Header = () => {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const { logout } = useAuth();
+    const history = useNavigate()
+
+    const accountSettingClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const accountSettingClose = () => {
+        setAnchorEl(null);
+    }
+
+    function signOut() {
+        logout().then(() => history('/login'));
+    }
 
     return (
         <>
             <div className={classes.contain}>
-                <AppBar position="static" color="secondary">
+                <AppBar position="static" color="transparent">
                     <Toolbar>
                         <Grid container>
                             <Grid item sm={4}>
                                 <InputBase placeholder="search" />
                             </Grid>
                             <Grid item sm={8}>
-                                <IconButton>
-                                    <Badge color="secondary" badgeContent={100}>
-                                        <NotificationsNoneIcon></NotificationsNoneIcon>
+                                <IconButton title="Notification">
+                                    <Badge color="error" badgeContent={1}>
+                                        <NotificationsNone />
                                     </Badge>
                                 </IconButton>
-                                <IconButton>
-                                    <Badge color="secondary" badgeContent={100}>
-                                        <MailOutlineIcon></MailOutlineIcon>
+                                <IconButton title="Mail">
+                                    <Badge color="error" badgeContent={4}>
+                                        <MailOutline />
                                     </Badge>
                                 </IconButton>
-                                <IconButton>
-                                    <Badge color="secondary" badgeContent={100}>
-                                        <PowerSettingsNewIcon></PowerSettingsNewIcon>
-                                    </Badge>
+                                <IconButton title="Accounts" onClick={accountSettingClick}>
+                                    <AccountCircle></AccountCircle>
                                 </IconButton>
+                                <Menu anchorEl={anchorEl} open={open} onClose={accountSettingClose} onClick={accountSettingClose}
+                                    PaperProps={{
+                                        sx: {
+                                            overflow: 'visible',
+                                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                            '&:before': {
+                                                content: '""',
+                                                display: 'block',
+                                                position: 'absolute',
+                                                top: 0,
+                                                right: 14,
+                                                width: 10,
+                                                height: 10,
+                                                bgcolor: 'background.paper',
+                                                transform: 'translateY(-50%) rotate(45deg)',
+                                                zIndex: 0,
+                                            },
+                                        },
+                                    }}
+                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                >
+                                    <MenuItem>
+                                        <ListItemIcon>
+                                            <AccountCircle />
+                                        </ListItemIcon>
+                                        Profile
+                                    </MenuItem>
+                                    <MenuItem>
+                                        <ListItemIcon>
+                                            <Settings />
+                                        </ListItemIcon>
+                                        Settings
+                                    </MenuItem>
+                                    <MenuItem onClick={signOut}>
+                                        <ListItemIcon>
+                                            <Logout />
+                                        </ListItemIcon>
+                                        Logout
+                                    </MenuItem>
+                                </Menu>
                             </Grid>
                         </Grid>
                     </Toolbar>
