2748302ba2e0b72229e6aa6eca23cdeba5e796e6
Al Zami Arafat <alzamiarafat00@gmail.com>
2021-11-28 15:31:41 +0600

    add redux presist

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import {store} from './store/store';
+import { store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './store/store';
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
   document.getElementById('root')
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,20 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import { rootReducer } from "./rootReducer";
 
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: getDefaultMiddleware,
     devTools: process.env.NODE_ENV !== "production"
 })
+
+export const persistor = persistStore(store);
